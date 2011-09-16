2cd09963fa6a98c908bdbd7bc4ea7f850a35916a
Mike Stetsyshyn <jays.demons@gmail.com>
2011-09-16 21:12:52 +0300

    comment cleanup

--- app/models/WorkHours.java
+++ app/models/WorkHours.java
@@ -5,8 +5,10 @@ import java.util.Map;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import org.hibernate.annotations.CollectionOfElements;
 import org.hibernate.annotations.Where;
 
 import play.db.jpa.Model;
@@ -19,27 +21,10 @@ public class WorkHours extends Model {
     public boolean                   deleted = false;
 
     public String                    description;
-    //@ManyToMany
+    
     //public Map<String, IrregularDay> irregularDays;
     public Day                       saturday;
     public Day                       sunday;
 
     public Day                       worrkDay;
-
-    public JsonObject getWorkHours() {
-        return null;
-    }
-
-//    public boolean isNowWorking() {
-//        if (irregularDays.get(new Date().toString()) != null) {
-//            IrregularDay irregularDay = irregularDays
-//                    .get(new Date().toString());
-//            if (irregularDay.from.compareTo(new Date().getHours() + "") > 0
-//                    && irregularDay.to.compareTo(new Date().getHours() + "") < 0) {
-//                return true;
-//            }
-//        }
-//
-//        return false;
-//    }
 }
