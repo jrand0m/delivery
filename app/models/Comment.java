a9e315cc3d78e23db10c7b091edf472483c13362
Mike Stetsyshyn <jays.demons@gmail.com>
2011-09-16 21:18:16 +0300

    Added option to show as anon for user

--- app/models/Comment.java
+++ app/models/Comment.java
@@ -6,6 +6,7 @@ package models;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToOne;
 
 import play.db.jpa.Model;
 
@@ -19,15 +20,19 @@ public class Comment extends Model {
     /**
      * allow only registered users !
      * */
+    @OneToOne
     public Order	order;
-    
+    @OneToOne
     public Client 	client;
     
     public String 	text;
     public Integer 	commonRating;
     public Date		date;
     public CommentStatus status = CommentStatus.NOT_REVIEWED;
-    
+    /**
+     * Based on user prefs set default, and ask if to hide his name;
+     * */
+    public boolean showAsAnonymous;
     public static enum CommentStatus {
 	
 	NOT_REVIEWED, APPROVED,  REJECTED
