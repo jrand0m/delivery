405518b0d77d584d0c7c5b944d6225a5fa7db1b6
Mike Stetyshyn <jays.demons@gmail.com>
2011-10-02 22:45:25 +0300

    admin page summary done

--- app/models/Order.java
+++ app/models/Order.java
@@ -69,6 +69,7 @@ public class Order extends GenericModel {
 		public static final String BY_SHORT_ID_OR_LIKE_FULL_ID = Order.FIELDS.SHORTHAND_ID
 				+ " = ? or " + Order.FIELDS.ID + " like ?";
 		public static final String BY_RESTAURANT_AND_STATUS = Order.FIELDS.RESTAURANT + " = ? and "+ Order.FIELDS.ORDER_STATUS + " = ? ";
+		public static final String BY_RESTAURANT_AND_STATUS_AND_AFTER_DATE = BY_RESTAURANT_AND_STATUS + " and " + FIELDS.ORDER_COOKED +  " > ?";
 	}
 
 	public static Order findByShortId(String shortID) {
