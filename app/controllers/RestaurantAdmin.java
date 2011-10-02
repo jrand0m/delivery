dd9baf0b7c6f9f0c364693a37b27c760bd237161
Mike Stetyshyn <jays.demons@gmail.com>
2011-10-02 20:26:08 +0300

    new

--- app/controllers/RestaurantAdmin.java
+++ app/controllers/RestaurantAdmin.java
@@ -1,54 +1,90 @@
 package controllers;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import models.Comment;
+import models.Order;
+import models.Restaurant;
+import models.users.RestaurantAdministration;
+import models.users.RestaurantUser;
+import models.users.User;
+import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.With;
 import annotations.Check;
+import enumerations.OrderStatus;
 import enumerations.UserRoles;
 
 @With(Secure.class)
 @Check(UserRoles.RESTAURANT_ADMIN)
 public class RestaurantAdmin extends Controller {
 
-	public static void summary() {
+	@Before
+	public static void _prepare(){
 		String login = Security.connected();
-		renderArgs.put("module", "summary");
-		render(login);
+		RestaurantAdministration user = RestaurantAdministration.find(User.HQL.BY_LOGIN, login).first();
+		notFoundIfNull(user);
+		renderArgs.put("user", user);
+		renderArgs.put("login", login);
+	}
+	
+	
+	public static void summary() {
+		
+		List<Order> lastOrders = null;
+		List<Comment> lastComments = null;
+		
+		renderArgs.put("lastComments", lastComments);
+		renderArgs.put("lastOrders", lastOrders);
+		render();
 	}
 
 	public static void showMenu() {
-		String login = Security.connected();
-		renderArgs.put("module", "showMenu");
-		render(login);
+		render();
 	}
 
 	public static void showReports() {
-		String login = Security.connected();
-		renderArgs.put("module", "showReports");
-		render(login);
+		render();
 	}
 
 	public static void showProfile() {
-		String login = Security.connected();
-		renderArgs.put("module", "showProfile");
-		render(login);
+		render();
 	}
 
 	public static void showEvents() {
-		String login = Security.connected();
-		renderArgs.put("module", "showEvents");
-		render(login);
+		render();
 	}
 
 	public static void showInvoices() {
-		String login = Security.connected();
-		renderArgs.put("module", "showInvoices");
-		render(login);
+		render();
 	}
 
 	public static void showShop() {
-		String login = Security.connected();
-		renderArgs.put("module", "showShop");
-		render(login);
+		render();
+	}
+	/*UTIL METHODS*/
+	public static void getSummaryChartData(){
+		
+		List<ChartDataHolder> chartData = new ArrayList<ChartDataHolder>();
+		List<Order> orders = Order.find(Order.HQL.BY_RESTAURANT_AND_STATUS, ((RestaurantAdministration)renderArgs.get("user")).restaurant, OrderStatus.DELIVERED).fetch();
+		for (Order order:orders){
+			ChartDataHolder holder = new ChartDataHolder(order.orderCooked, String.valueOf(order.totalMenuPrice - (order.totalMenuPrice* order.restaurantDiscount)));
+			chartData.add(holder);
+		}
+		renderTemplate("RestaurantAdmin/summaryChartData.js", chartData);
+	}
+	/*helper classes */
+	public static class ChartDataHolder {
+		public String date;
+		public String value;
+		ChartDataHolder(Date date, String value){
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy,MM,dd");
+			this.date = sdf.format(date);
+			this.value = value;
+		}
 	}
 
 }
