334b22ff3ed421776669910b9bb994d63737b23e
Andi <aaa@aaa>
2011-10-02 20:20:07 +0300

    erfrfrf

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -8,7 +8,13 @@ $(document).ready(function() {
 				$('p.selText').text($(this).text());
 				$('ul.selDrop li').removeClass('selected');
 				$(this).addClass('selected');
-				console.log($(this));
+				
+				var olo = $.cookie('PLAY_SESSION',{ raw: true });
+				
+				olo = olo.replace( new RegExp('(city%3A)[0-9]'), 'city%3A'+$(this).context.id );
+				$.cookie('PLAY_SESSION', olo, { raw: true,expires: 7} );
+				
+				window.location.href = document.location;
 				$('ul.selDrop').slideToggle();
 			});
 });
