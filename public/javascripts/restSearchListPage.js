303c2e200011b91273d84127f645b1108918c1c7
Andi <aaa@aaa>
2011-10-08 22:35:15 +0300

    тіпа там яваскріпти всякі

--- public/javascripts/restSearchListPage.js
+++ public/javascripts/restSearchListPage.js
@@ -7,10 +7,33 @@
 		$(this).next('ul.selDrop').slideToggle();
 	});
 	
-	$("ul.selDrop li").click(function(){
+	$("ul#selDrop1 li").click(function(){
+		$(this).parent().prev('p.selText').text($(this).text());
+		$(this).parent('li').removeClass('selected');
+		$(this).addClass('selected');
+		window.location.href = cngcty({id:$(this).context.id});
+		$(this).parent().slideToggle();
+	});
+	
+	$("ul#selDrop2 li, ul#selDrop3 li").click(function(){
+		$(this).parent().prev('p.selText').text($(this).text());
+		$(this).parent('li').removeClass('selected');
+		$(this).addClass('selected');
+		$("#rest-sort-list div.all").hide();
+		$("#rest-sort-list div." +$(this).context.id + "").show();
+		$(this).parent().slideToggle();
+	});
+
+	
+	
+	
+	
+	/*$("ul.selDrop li").click(function(){
 		$(this).parent().prev('p.selText').text($(this).text());
 		$(this).parent('li').removeClass('selected');
 		$(this).addClass('selected');
 		$(this).parent().slideToggle();
-	});
+	});*/
+	
+	
 });
