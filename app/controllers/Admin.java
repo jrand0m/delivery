91b3ebdf734d91b5aac61f3c28e1ab0eb67d746c
Mike Stetyshyn <jays.demons@gmail.com>
2011-11-02 01:17:14 +0200

    blejat

--- app/controllers/Admin.java
+++ app/controllers/Admin.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.Restaurant;
+import models.geo.City;
 import models.users.SystemAdministrator;
 import annotations.Check;
 import play.mvc.Controller;
@@ -9,6 +11,37 @@ import play.mvc.With;
 @Check(SystemAdministrator.class)
 public class Admin extends Controller {
 	public static void index(){
+		render();
+	}
+	public static void showCities(){
 		
 	}
+	public static void addCity(City city){
+		
+	}
+	public static void editCity(Long id, City city){
+		
+	}
+	public static void deleteCity(){
+		
+		flash.put("result", "cannot delete city '' because there is some restaurants assigned");
+		index();
+	}
+	public static void showCategories(){}
+	
+	public static void showRestaurants(Long cityid){
+		
+	}
+	public static void addRestaurant(Restaurant restaurant, Long catid, Long cityid){
+		
+	}
+	public static void editRestaurant(Long id, Restaurant restaurant){
+		
+	}
+	public static void deleteRestaurant(){
+		
+		flash.put("result", "cannot delete restaurant '' because there is some orders assigned");
+		index();
+	}
+	
 }
