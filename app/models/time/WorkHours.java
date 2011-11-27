e6574e123d2377f0e4d1b4e5c98736c318efc59d
Mike <jays.demons@gmail.com>
2011-11-27 23:52:13 +0200

    right sequence

--- app/models/time/WorkHours.java
+++ app/models/time/WorkHours.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 import org.hibernate.annotations.Type;
 import org.joda.time.LocalTime;
@@ -12,10 +14,12 @@ import play.db.jpa.GenericModel;
 import play.db.jpa.Model;
 
 @Entity
+@Table(name="Restaurant_Workhours")
 public class WorkHours extends GenericModel {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "workhours_seq")
+    @SequenceGenerator(name = "workhours_seq", sequenceName = "workhours_seq")
 	public Long id;
     
 	@Type(type="org.joda.time.contrib.hibernate.PersistentLocalTimeAsTime")
