59ca0b67fd24c371605e29979df0b3cf2438ef9f
Mike <jays.demons@gmail.com>
2012-01-25 01:36:44 +0200

    Guice based mybatis config

---
+++ app/guice/ApplicationModule.java
@@ -0,0 +1,17 @@
+package guice;
+
+import com.google.inject.AbstractModule;
+/**
+ * Intended to use in DAO classes to be able easily 
+ * create mocks for services and etc.
+ * 
+ * @author mike
+ * */
+public class ApplicationModule extends AbstractModule {
+
+	@Override
+	protected void configure() {
+
+	}
+	
+}
---
+++ app/guice/MyBatisConfigModule.java
@@ -0,0 +1,12 @@
+package guice;
+
+import org.mybatis.guice.MyBatisModule;
+
+public class MyBatisConfigModule extends MyBatisModule {
+    
+    @Override
+    protected void initialize() {
+        
+        
+    }
+}
