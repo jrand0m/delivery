5ea0b82ac428a6016561d9c19925ce899d596cb1
andriysmtp@gmail.com <andriysmtp@gmail.com>
2012-01-26 18:11:38 +0200

    design fix :)

--- public/javascripts/inputs_and_labels.js
+++ public/javascripts/inputs_and_labels.js
@@ -3,7 +3,9 @@ $(document).ready(function() {
 	var offset = $("#basket").offset();
 	var topPadding = 15;
 	$(window).scroll(function() {
-		if ($(window).scrollTop() > offset.top) {
+		
+			
+		if ($(window).scrollTop()> offset.top) {
 			$("#basket").stop().animate({marginTop: $(window).scrollTop() - offset.top + topPadding});
 		}
 		else {$("#basket").stop().animate({marginTop: 0});};});
