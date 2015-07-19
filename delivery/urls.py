f695101931758a87caea246736d4ea8e12f15fd8
Mike Stetsyshyn <jays.demons@gmail.com>
2015-07-19 21:57:48 +0300

    new design

--- delivery/urls.py
+++ delivery/urls.py
@@ -10,9 +10,8 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^pages/', include('django.contrib.flatpages.urls')),
-    url(r'^IndeX$', 'delivery.website.views.index', name='index'),
+    url(r'^test-index$', 'delivery.website.views.index', name='index'),
     url(r'^$', 'delivery.website.views.yoda', name='yoda'),
-    url(r'^for/restaurants', 'delivery.website.views.for_restaurants', name='for_restaurants'),
     
     url(r'^google9049961bc731c473.html', 'delivery.website.views.google', name='google'),
 )
---
+++ delivery/website/static/js/js.js
@@ -0,0 +1,4 @@
+$(".main-tabs li").click(function() {
+    $(".main-tabs li").removeClass("active")
+    $(this).addClass("active");
+});
