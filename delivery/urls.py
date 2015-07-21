9a47455a5d3840167c72ecb49812c794cbbf4a2a
Andrew Dunai <anderson.dunai@gmail.com>
2015-07-21 21:18:02 +0300

    Added test_500 page

--- delivery/urls.py
+++ delivery/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns(
 
     url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^test-index$', 'delivery.website.views.index', name='index'),
+    url(r'^test-500$', 'delivery.website.views.test_500', name='test_500'),
     url(r'^$', 'delivery.website.views.yoda', name='yoda'),
     
     url(r'^google9049961bc731c473.html', 'delivery.website.views.google', name='google'),
